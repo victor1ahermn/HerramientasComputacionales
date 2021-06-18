0d1e5d6deedcd25f367e593c8bbe49c4524ae50b
victor1ahermn <86083000+victor1ahermn@users.noreply.github.com>
2021-06-17 21:03:09 -0500

    subir archivos
    Se suben los archivos mencionados en el README

---
+++ problema2.py
@@ -0,0 +1,22 @@
+
+#Problema 2
+
+def triangulo(a,b,c):
+    ptriangulo=(a+b+c)
+    print(ptriangulo)
+    return ptriangulo
+
+def cuadrado(x):
+    pcuadrado=x*4
+    print(pcuadrado)
+    return pcuadrado
+
+def rectangulo(a,b):
+    prectangulo=2*(a+b)
+    print(prectangulo)
+    return prectangulo
+
+def circulo(r):
+    pcirculo=pi*2*r
+    print(pcirculo)
+    return pcirculo
